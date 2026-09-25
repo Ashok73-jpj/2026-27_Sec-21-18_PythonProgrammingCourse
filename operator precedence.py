79428f71ce1ae5dc71b39cdf5e729d6d98ddd71d
Ashok73-jpj <prasannalingala82@gmail.com>
2026-09-25 15:27:25 +0530

    Add files via upload

---
+++ operator precedence.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first value: "))
+b = int(input("Enter second value: "))
+c = int(input("Enter third value: "))
+
+x = a ** b ** c
+
+print(f"{a} + {b} * {c} = {a + b * c}")
+print(f"{a} ** {b} ** {c} = {a ** b ** c}")
+print(f"{a} - {b} - {c} = {a - b - c}")
